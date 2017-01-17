f822b8819fc6c1f329a32706b1fdc59e0161d1cf
Ed <ed@breachintelligence.com>
2017-01-16 21:48:18 -0500

    Update admin-only and user-can-edit flags to be camelCased

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
      * @type String
      * @required
      */
-    "name": "VirusTotal",
+    name: "VirusTotal",
     /**
      * The acronym that appears in the notification window when information from this integration
      * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
@@ -15,14 +15,14 @@ module.exports = {
      * @type String
      * @required
      */
-    "acronym":"VT",
+    acronym: "VT",
     /**
      * Description for this integration which is displayed in the Polarity integrations user interface
      *
      * @type String
      * @optional
      */
-    "description": "VirusTotal Integration for File and IP Address Reports",
+    description: "VirusTotal Integration for File and IP Address Reports",
     /**
      * An array of style files (css or less) that will be included for your integration. Any styles specified in
      * the below files can be used in your custom template.
@@ -41,14 +41,26 @@ module.exports = {
      * @type Object
      * @optional
      */
-    // "block": {
-    //     "component": {
-    //         "file": "./components/sample_integration.js"
-    //     },
-    //     "template": {
-    //         "file": "./templates/sample_integration.hbs"
-    //     }
-    // },
+    block: {
+        component: {
+            file: "./components/virustotal.js"
+        },
+        template: {
+            file: "./templates/virustotal.hbs"
+        }
+    },
+    logging:{
+        // directory is relative to the this integrations directory
+        // e.g., if the integration is in /app/polarity-server/integrations/virustotal
+        // and you set directoryPath to be `integration-logs` then your logs will go to
+        // `/app/polarity-server/integrations/integration-logs`
+        // You can also set an absolute path.  If you set an absolute path you must ensure that
+        // the directory you specify is writable by the `polarityd:polarityd` user and group.
+
+        //directoryPath: '/var/log/polarity-integrations',
+        level: 'info',  //trace, debug, info, warn, error, fatal
+        fileName: 'virustotal.log'
+    },
     /**
      * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
      * as an array of option objects.
@@ -103,7 +115,7 @@ module.exports = {
              * @property user-can-edit
              * @type Boolean
              */
-            "user-can-edit" : true,
+            "userCanEdit" : true,
             /**
              * If set to true, the setting can only be viewed by admins.  For all other users the setting will not appear.
              * Note that if `admin-only` is set to true the value of `user-can-edit` is not applicable.
@@ -111,7 +123,25 @@ module.exports = {
              * @property admin-only
              * @type Boolean
              */
-            "admin-only"    : false
+            "adminOnly"    : false
+        },
+        {
+            "key"         : "lookupIps",
+            "name"        : "Lookup IP Addresses",
+            "description" : "If checked, the integration will lookup IP addresses in VirusTotal",
+            "default"     : true,
+            "type"        : "boolean",
+            "userCanEdit" : true,
+            "adminOnly"    : false
+        },
+        {
+            "key"         : "lookupFiles",
+            "name"        : "Lookup Files (Hashes)",
+            "description" : "If checked, the integration will lookup files (MD5, SHA1, SHA256) in VirusTotal",
+            "default"     : true,
+            "type"        : "boolean",
+            "userCanEdit" : true,
+            "adminOnly"    : false
         },
         {
             "key"         : "warnOnLookupLimit",
@@ -120,8 +150,8 @@ module.exports = {
             " VirusTotal API key lookup limit",
             "default"     : false,
             "type"        : "boolean",
-            "user-can-edit" : true,
-            "admin-only"    : false
-        },
+            "userCanEdit" : true,
+            "adminOnly"    : false
+        }
     ]
 };
