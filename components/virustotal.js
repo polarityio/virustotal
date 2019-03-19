5a9ab45dde6895f495d06a7f2c16284772492c73
Ed <ed@polarity.io>
2019-03-19 13:45:41 -0400

    Reformat and add timezone

--- components/virustotal.js
+++ components/virustotal.js
@@ -1,27 +1,29 @@
 polarity.export = PolarityComponent.extend({
-    classNames: ['virustotal-details-block'],
-    maxResolutionsToShow: 10,
-    maxUrlsToShow: 10,
-    numResolutionsShown: Ember.computed('maxResolutionsToShow', 'block.data.details.resolutions.length', function(){
-        var maxResolutionsToShow = this.get('maxResolutionsToShow');
-        var totalResolutions = this.get('block.data.details.resolutions.length');
+  timezone: Ember.computed('Intl', function() {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
+  }),
+  maxResolutionsToShow: 10,
+  maxUrlsToShow: 10,
+  numResolutionsShown: Ember.computed('maxResolutionsToShow', 'block.data.details.resolutions.length', function() {
+    let maxResolutionsToShow = this.get('maxResolutionsToShow');
+    let totalResolutions = this.get('block.data.details.resolutions.length');
 
-        if(maxResolutionsToShow < totalResolutions){
-            return maxResolutionsToShow;
-        }
-        return totalResolutions;
-    }),
-    numUrlsShown: Ember.computed('maxUrlsToShow', 'block.data.details.detectedUrls.length', function(){
-        var maxUrlsToShow = this.get('maxUrlsToShow');
-        var totalUrls = this.get('block.data.details.detectedUrls.length');
+    if (maxResolutionsToShow < totalResolutions) {
+      return maxResolutionsToShow;
+    }
+    return totalResolutions;
+  }),
+  numUrlsShown: Ember.computed('maxUrlsToShow', 'block.data.details.detectedUrls.length', function() {
+    let maxUrlsToShow = this.get('maxUrlsToShow');
+    let totalUrls = this.get('block.data.details.detectedUrls.length');
 
-        if(maxUrlsToShow < totalUrls){
-            return maxUrlsToShow;
-        }
-        return totalUrls;
-    }),
-    ipVirusTotalLink: Ember.computed('block.entity.value', function(){
-        var entityName = this.get('block.entity.value');
-        return 'https://www.virustotal.com/en/ip-address/' + entityName + '/information/';
-    })
+    if (maxUrlsToShow < totalUrls) {
+      return maxUrlsToShow;
+    }
+    return totalUrls;
+  }),
+  ipVirusTotalLink: Ember.computed('block.entity.value', function() {
+    let entityName = this.get('block.entity.value');
+    return 'https://www.virustotal.com/en/ip-address/' + entityName + '/information/';
+  })
 });
